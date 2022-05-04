13831e92e60f708fc67039cee2dde07e6c0a0f36
Владлен Шапарев <shve@outlook.com>
2022-05-04 07:17:47 +0700

    Add project files.

---
+++ ArrayOperation.cs
@@ -0,0 +1,73 @@
+﻿namespace OneDimensionalArray
+{
+    internal static class ArrayOperation
+    {
+        internal static (int[] even, int[] odd) GetOddAndEvenSubarrays(int[] incomingArray)
+        {
+            var evenList = new List<int>();
+            var oddList = new List<int>();
+            foreach (var element in incomingArray)
+            {
+                var temp = element % 2;
+                if (temp == 0)
+                {
+                    evenList.Add(element);
+                    continue;
+                }
+                if (temp == 1 || temp == -1)
+                {
+                    oddList.Add(element);
+                    continue;
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong incoming array.");
+                }
+            }
+            evenList.Sort();
+            oddList.Sort();
+            return (evenList.ToArray(), oddList.ToArray());
+        }
+
+        internal static int GetDifOfMinMax(int[] incomingArray)
+        {
+            return incomingArray.Max() - incomingArray.Min();
+        }
+
+        internal static bool IsArrayArranged(int[] incomingArray)
+        {
+            var length = incomingArray.Length;
+            if (incomingArray[0] < incomingArray[^0])
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (incomingArray[i - 1] > incomingArray[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (incomingArray[0] > incomingArray[^0])
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (incomingArray[i - 1] < incomingArray[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (incomingArray[0] == incomingArray[^0])
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (incomingArray[i - 1] != incomingArray[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System;
+
+var intArray = new int[] { 1, 2, 3, 4, 5, -12,7,0 };
+Array.Sort(intArray);
+foreach (var element in intArray)
+{
+    Console.WriteLine(element);
+}
